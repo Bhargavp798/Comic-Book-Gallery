dcc853515093c1f7f51b1762309117e85b2b5f86
Bhargav <balabhargav07@gmail.com>
2017-09-19 15:11:56 -0500

    Added a Controller and Action with name Detail to Display Hello MVC Project!

---
+++ ComicBookDetails/Controllers/ComicGalleryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookDetails.Controllers
+{
+    public class ComicGalleryController:Controller
+    {
+        public string Detail()
+        {
+            return "Hello MVC Project!";
+        }
+    }
+}
