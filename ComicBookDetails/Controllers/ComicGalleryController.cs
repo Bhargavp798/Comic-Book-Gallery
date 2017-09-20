58305dfdc5c43ad5742c364b6510b7e27da78456
Bhargav <balabhargav07@gmail.com>
2017-09-20 12:35:58 -0500

    Replaced the variables in controller and added viewbag in the view.

--- ComicBookDetails/Controllers/ComicGalleryController.cs
+++ ComicBookDetails/Controllers/ComicGalleryController.cs
@@ -10,6 +10,19 @@ namespace ComicBookDetails.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.Name = "Half Girlfriend";
+            ViewBag.BookPrice = 700;
+            ViewBag.BookDescription = "<p>Chetan Bhagat published his first novel first novel 'Five Point Someone' in 2004 and this very first venture took him to the peaks of fame and popularity. The book depicted the story of an IIT student who considers himself to be below average than all the other students in IIT.</p>";
+            ViewBag.Authors = new string[]
+            {
+        "Chetan Bhagat:Author",
+        "Naga Deepika: Assosiate Writer",
+        "Bala Bhargav: Chief Executive",
+        "Randeep Singh: Editor Lineup",
+        "Srinivas Rao:Producer Of Book"
+
+            };
+
             return View();
         }   
     }
