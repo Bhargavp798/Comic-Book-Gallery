83e1afa9201e19d8b2fdb69eefa4d67a0c4ebbc2
Bhargav <balabhargav07@gmail.com>
2017-09-21 14:32:25 -0500

    Added The model to the application

---
+++ ComicBookDetails/Models/AuthorsMod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookDetails.Models
+{
+    public class AuthorsMod
+    {
+        public string Role { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ComicBookDetails/Models/ComicBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookDetails.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesName { get; set; }
+        public string DescriptionHtml { get; set; }
+        public int IssueNumber { get; set; }
+        public AuthorsMod[] Authors { get; set; }
+        public bool Favourite { get; set; }
+
+
+    }
+}
