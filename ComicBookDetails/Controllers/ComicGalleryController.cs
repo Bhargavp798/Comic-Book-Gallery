1f9afffc18843ef91fd72c9b6fa8e4b49c030261
Bhargav <balabhargav07@gmail.com>
2017-09-21 22:50:25 -0500

    Added Model attributes to Controller and Then Controller to View.

--- ComicBookDetails/Controllers/ComicGalleryController.cs
+++ ComicBookDetails/Controllers/ComicGalleryController.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookDetails.Models;
 
 namespace ComicBookDetails.Controllers
 {
     public class ComicGalleryController:Controller
     {
+        public object Role { get; private set; }
+
         public ActionResult Detail()
         {
-            ViewBag.Name = "Half Girlfriend";
-            ViewBag.BookPrice = 700;
-            ViewBag.BookDescription = "<p>Chetan Bhagat published his first novel first novel 'Five Point Someone' in 2004 and this very first venture took him to the peaks of fame and popularity. The book depicted the story of an IIT student who considers himself to be below average than all the other students in IIT.</p>";
-            ViewBag.Authors = new string[]
+            var ComicBook = new ComicBook()
             {
-        "Chetan Bhagat:Author",
-        "Naga Deepika: Assosiate Writer",
-        "Bala Bhargav: Chief Executive",
-        "Randeep Singh: Editor Lineup",
-        "Srinivas Rao:Producer Of Book"
-
+                SeriesName = "Half Girlfriend",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Chetan Bhagat published his first novel first novel 'Five Point Someone' in 2004 and this very first venture took him to the peaks of fame and popularity.The book depicted the story of an IIT student who considers himself to be below average than all the other students in IIT.</p>",
+                Authors = new AuthorsMod[]
+                {
+                    new AuthorsMod() {Role="Author",Name="Chetan Bhagat"},
+                    new AuthorsMod() {Role="Assosiate Writer",Name="Naga Deepika"},
+                    new AuthorsMod() {Role="Chief Executive",Name="Bala Bhargav"},
+                    new AuthorsMod() {Role="Editor Lineup",Name="Randeep Singh"},
+                    new AuthorsMod() {Role="Producer",Name="Srinivas Rao"}
+                }
             };
-
-            return View();
+        return View(ComicBook);
         }   
     }
 }
